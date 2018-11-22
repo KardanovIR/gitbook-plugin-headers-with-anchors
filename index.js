fb4d81e69ccd2e30064f89811bc82877356b674a
Shushik <silkleopard@yandex.ru>
2018-11-22 17:14:33 +0300

    Trying to use hooks

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ function testTag1(block) {
     return 'TESTTESTTEST' + block.body + 'TESTTESTTEST';
 }
 
+function hookForPage(page) {
+    data.content = 'TESTTESTTEST' + data.content + 'TESTTESTTEST';
+}
+
 module.exports = {
     blocks: {
         testTag1
+    },
+    hooks: {
+        page: hookForPage
     }
 };
