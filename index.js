279495558beae82600582c6bd7ee045d6a320774
Shushik <silkleopard@yandex.ru>
2018-11-22 17:56:50 +0300

    So... let's try to make some funny things

--- index.js
+++ index.js
@@ -2,7 +2,46 @@ function testTag1(block) {
     return 'TESTTESTTEST' + block.body + 'TESTTESTTEST';
 }
 
-function hookForPage(page) {
+const
+    HEADER_BEG = '<h[1-6] id="([^"]+)">',
+    HEADER_END = '<\\/h[1-6]>',
+    HEADER_INNER = '[^>]+';
+
+function addAnchorToHeader(raw) {
+    var
+        id = raw.
+             replace(new RegExp(HEADER_BEG + HEADER_INNER + HEADER_END), '$1'),
+        out = raw.
+              replace(new RegExp(HEADER_BEG), '').
+              replace(new RegExp(HEADER_END), '');
+
+    out = id;
+
+    return out;
+}
+
+function addAnchorsToHeaders(content) {
+
+    var
+        rexp = new RegExp(HEADER_BEG + HEADER_INNER + HEADER_END, 'g'),
+        headers = content.match(rexp);
+
+    // No need to go further
+    if (!(headers instanceof Array) || !headers.length) {
+        return;
+    }
+
+    // Replace
+    headers.forEach((header) => {
+        content = content.replace(header, addAnchorsToHeaders(header));
+    });
+
+    return content;
+}
+
+function page(page) {
+    page.content = addAnchorsToHeaders(page.content);
+
     page.content = page.content.replace(/<(\\?)h1>/g, '<$1h3>');
     return page;
 }
@@ -12,6 +51,6 @@ module.exports = {
         testTag1
     },
     hooks: {
-        page: hookForPage
+        page
     }
 };
