7512f3c1ee7bb847a2d3c3386cdd941dfd2fb43f
Shushik <silkleopard@yandex.ru>
2018-11-23 13:40:21 +0300

    Seems like it should work properly

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-function testTag1(block) {
-    return 'TESTTESTTEST' + block.body + 'TESTTESTTEST';
-}
-
+/**
+ * @const {string} TEXT_HEADER_BEG
+ * @const {string} TEXT_HEADER_END
+ * @const {string} REXP_HEADER_BEG
+ * @const {string} REXP_HEADER_END
+ * @const {string} REXP_HEADER_INNER
+ */
 const
-    TEXT_HEADER_BEG = '<h$1 id="$2"><a id="#anchor-{{ id }}" href="#anchor-{{ id }}">',
+    TEXT_HEADER_BEG = '<h$1$2><a ' +
+                          'id="#anchor-{{ id }}" ' +
+                          'class="wd-go wd-go_for_header-anchor" ' +
+                          'href="#anchor-{{ id }}"' +
+                      '>',
     TEXT_HEADER_END = '</a></h$1>',
-    REXP_HEADER_BEG = '<h([1-6]) id="([^"]+)">',
+    REXP_HEADER_BEG = '<h([1-6])(\s+id="[^"]+")?>',
     REXP_HEADER_END = '<\\/h([1-6])>',
-    REXP_HEADER_INNER = '[^>]+';
+    REXP_HEADER_INNER = '([^>]+)';
 
+/**
+ * @function {addAnchorToHeader}
+ *
+ * @param {string} raw
+ */
 function addAnchorToHeader(raw) {
     var
-        n = raw.
-             replace(new RegExp(REXP_HEADER_BEG + REXP_HEADER_INNER + REXP_HEADER_END), '$1'),
-        id = raw.
-             replace(new RegExp(REXP_HEADER_BEG + REXP_HEADER_INNER + REXP_HEADER_END), '$2').
-             replace(/[()]/g, '-').
+        id = encodeURIComponent(raw.
+             replace(new RegExp(REXP_HEADER_BEG + REXP_HEADER_INNER + REXP_HEADER_END), '$3').
+             replace(/[\s()\[\]"'`.,:;?!#&]/g, '-').
              replace(/-+/g, '-').
-             toLowerCase(),
-        out = raw.
-              replace(new RegExp(REXP_HEADER_BEG), TEXT_HEADER_BEG).
-              replace(new RegExp(REXP_HEADER_END), TEXT_HEADER_END).
-              replace(/\{\{\s*id\s*\}\}/g, id);
+             toLowerCase()),
+        out = '';
+
+    out = raw.
+          replace(new RegExp(REXP_HEADER_BEG), TEXT_HEADER_BEG).
+          replace(new RegExp(REXP_HEADER_END), TEXT_HEADER_END).
+          replace(/\{\{\s*id\s*\}\}/g, id);
 
     return out;
 }
 
+/**
+ * @function {addAnchorsToHeaders}
+ *
+ * @param {string} raw
+ */
 function addAnchorsToHeaders(raw) {
     var
         rexp = new RegExp(REXP_HEADER_BEG + REXP_HEADER_INNER + REXP_HEADER_END, 'g'),
@@ -44,19 +61,23 @@ function addAnchorsToHeaders(raw) {
     return raw;
 }
 
+/**
+ * @function {page}
+ *
+ * @param {object} page
+ */
 function page(page) {
     page.content = addAnchorsToHeaders(page.content);
-
-console.log(page.content);
 
     return page;
 }
 
+
+
 module.exports = {
-    blocks: {
-        testTag1
-    },
+    assets: './assets',
+    css: ['index.css'],
     hooks: {
         page
-    }
+    },
 };
